ceeb871a393ff24eb8976deab2e8071807794624
elysiumsurr6-glitch <elysiumsurr6@gmail.com>
2025-10-16 00:42:50 +0200

    Add files via upload

---
+++ RLSDK/SdkHeaders.hpp
@@ -0,0 +1,57 @@
+/*
+#############################################################################################
+# Rocket League SDK (RLSDK) Season 20 (v2.59) 10/14/2025 05:30PM
+# Generated with the CodeRedGenerator v1.1.5
+# ========================================================================================= #
+# File: SdkHeaders.hpp
+# ========================================================================================= #
+# Credits: ItsBranK, TheFeckless, SSLow
+# Links: www.github.com/CodeRedModding/CodeRed-Generator, discord.gg/d5ahhQmJbJ
+#############################################################################################
+*/
+#pragma once
+
+/*
+# ========================================================================================= #
+# Includes
+# ========================================================================================= #
+*/
+
+#include "GameDefines.hpp"
+#include "SDK_HEADERS/Core_structs.hpp"
+#include "SDK_HEADERS/Core_classes.hpp"
+#include "SDK_HEADERS/Core_parameters.hpp"
+#include "SDK_HEADERS/Engine_structs.hpp"
+#include "SDK_HEADERS/Engine_classes.hpp"
+#include "SDK_HEADERS/Engine_parameters.hpp"
+#include "SDK_HEADERS/IpDrv_structs.hpp"
+#include "SDK_HEADERS/IpDrv_classes.hpp"
+#include "SDK_HEADERS/IpDrv_parameters.hpp"
+#include "SDK_HEADERS/XAudio2_structs.hpp"
+#include "SDK_HEADERS/XAudio2_classes.hpp"
+#include "SDK_HEADERS/XAudio2_parameters.hpp"
+#include "SDK_HEADERS/GFxUI_structs.hpp"
+#include "SDK_HEADERS/GFxUI_classes.hpp"
+#include "SDK_HEADERS/GFxUI_parameters.hpp"
+#include "SDK_HEADERS/AkAudio_structs.hpp"
+#include "SDK_HEADERS/AkAudio_classes.hpp"
+#include "SDK_HEADERS/AkAudio_parameters.hpp"
+#include "SDK_HEADERS/WinDrv_structs.hpp"
+#include "SDK_HEADERS/WinDrv_classes.hpp"
+#include "SDK_HEADERS/WinDrv_parameters.hpp"
+#include "SDK_HEADERS/OnlineSubsystemEOS_structs.hpp"
+#include "SDK_HEADERS/OnlineSubsystemEOS_classes.hpp"
+#include "SDK_HEADERS/OnlineSubsystemEOS_parameters.hpp"
+#include "SDK_HEADERS/ProjectX_structs.hpp"
+#include "SDK_HEADERS/ProjectX_classes.hpp"
+#include "SDK_HEADERS/ProjectX_parameters.hpp"
+#include "SDK_HEADERS/TAGame_structs.hpp"
+#include "SDK_HEADERS/TAGame_classes.hpp"
+#include "SDK_HEADERS/TAGame_parameters.hpp"
+#include "SDK_HEADERS/Extras.hpp"
+
+/*
+# ========================================================================================= #
+#
+# ========================================================================================= #
+*/
